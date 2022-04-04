7cc78cfd6747346f725601dc694a5bebe01c1043
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2022-04-04 08:21:56 -0400

    Speed up transition and support terra

--- R/lcp_tools.R
+++ R/lcp_tools.R
@@ -170,18 +170,81 @@ start_end_points = function(centerline, param)
   return(list(A = A, B = B))
 }
 
-transition <- function(conductivity)
+#' @importClassesFrom  gdistance TransitionLayer
+transition <- function(conductivity, directions = 8, geocorrection = TRUE)
 {
   verbose("Computing graph map...\n")
 
-  trans <- gdistance::transition(conductivity, transitionFunction = mean, directions = 8)
+  x = conductivity
+  use_terra = FALSE
+
+  if (is(x, "RasterLayer"))
+  {
+    bb = raster::extent(x)
+    ncells = raster::ncell(x)
+    val = raster::values(x)
+  }
+  else if (is(x, "SpatRaster"))
+  {
+    bb = terra::ext(x)
+    bb = raster::extent(bb[1:4])
+    ncells = terra::ncell(x)
+    val = terra::values(x)[,1]
+    use_terra = TRUE
+  }
+  else
+  {
+    stop("Only RasterLayer and SpatRaster are supported")
+  }
+
+  symm = TRUE
+
+  tr <- new("TransitionLayer",
+            nrows=as.integer(nrow(x)),
+            ncols=as.integer(ncol(x)),
+            extent=bb,
+            crs=sp::CRS(),
+            transitionMatrix = Matrix::Matrix(0, ncells,ncells),
+            transitionCells = 1:ncells)
+
+  transitionMatr <- gdistance::transitionMatrix(tr)
+  Cells <- which(!is.na(val))
+
+  if (use_terra)
+    adj <- terra::adjacent(x, cells=Cells, pairs=TRUE, directions=directions)
+  else
+    adj = raster::adjacent(x, cells=Cells, pairs=TRUE, directions=directions)
+
+  if(symm)
+    adj <- adj[adj[,1] < adj[,2],]
+
+  dataVals <- cbind(val[adj[,1]], val[adj[,2]])
+  transition.values <- rowMeans(dataVals)
+
+  if(!all(transition.values>=0))
+    warning("transition function gives negative values")
+
+  transitionMatr[adj] <- as.vector(transition.values)
+  if(symm)
+    transitionMatr <- Matrix::forceSymmetric(transitionMatr)
+
+  gdistance::transitionMatrix(tr) <- transitionMatr
+  gdistance::matrixValues(tr) <- "conductance"
+
+
+  #trans <- gdistance::transition(conductivity, transitionFunction = mean, directions = 8)
+  trans = tr
   verbose("   - Transition graph\n")
 
-  trans <- gdistance::geoCorrection(trans)
-  verbose("   - Geocorrection graph\n")
+  if (isTRUE(geocorrection))
+  {
+    trans <- gdistance::geoCorrection(trans)
+    verbose("   - Geocorrection graph\n")
+  }
 
   return(trans)
 }
+
 
 find_path = function(trans, centerline, A, B, param)
 {
